781a119274bb1e5c49d94d0ed746d93be2219802
Uday Katale <kataleuday@gmail.com>
2026-08-29 23:42:30 +0530

    Time: 4 ms (82.55%) | Memory: 42 MB (68.02%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,18 +1,34 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long left=0;
-        long right=(long) Math.sqrt(c);
+        // long left=0;
+        // long right=(long) Math.sqrt(c);
 
-        while(left<=right){
-            long sum= (left*left) + (right*right);
-            if(sum==c){
-                return true;
-            }else if(sum<c){
-                left++;
-            }else{
-                right--;
+        // while(left<=right){
+        //     long sum= (left*left) + (right*right);
+        //     if(sum==c){
+        //         return true;
+        //     }else if(sum<c){
+        //         left++;
+        //     }else{
+        //         right--;
+        //     }
+        // }
+        // return false;
+        long left = 0;
+        long right = (long) Math.sqrt(c);
+        
+        while (left <= right) {
+            long currentSum = (left * left) + (right * right);
+            
+            if (currentSum == c) {
+                return true; // Match found!
+            } else if (currentSum < c) {
+                left++; // Sum is too small, shift left pointer up
+            } else {
+                right--; // Sum is too big, shift right pointer down
             }
         }
+        
         return false;
     }
 }
