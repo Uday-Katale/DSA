952783272563c28078beeea52c6d6537392fcb0c
Uday Katale <kataleuday@gmail.com>
2026-08-29 14:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (54.26%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         boolean [] ar= new boolean[128];
-        for(char ch : jewels.toCharArray()){
-            ar[ch]=true;
+        for(int i =0; i<jewels.length(); i++){
+            ar[jewels.charAt(i)]=true;
         }
         int count=0;
-        for( char ch : stones.toCharArray()){
-            if(ar[ch]){
+        for(int i =0;i <stones.length(); i++){
+            if(ar[stones.charAt(i)]){
                 count++;
             }
         }
