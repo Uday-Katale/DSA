fcaacba9b68cd65a4793813ae2893c2734d99e8e
Uday Katale <kataleuday@gmail.com>
2026-08-29 11:16:59 +0530

    Time: 4 ms (22.98%) | Memory: 269 MB (6.69%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,10 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k %=nums.length;
+        if(k<0){
+            k+=nums.length;
+        }
         reverse(nums, 0, nums.length-1);
         reverse(nums, 0,k-1);
         reverse(nums, k, nums.length-1);
     }
+
     public static void reverse(int[] nums, int left, int right){
         while(left<right){
             int temp= nums[left];
@@ -13,6 +17,5 @@ class Solution {
             left++;
             right--;
         }
-
     }
 }
