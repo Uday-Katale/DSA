13838478202799ddcac5a57841c328ed29ee831d
Uday Katale <kataleuday@gmail.com>
2026-08-29 23:42:00 +0530

    Time: 4 ms (82.55%) | Memory: 42.4 MB (5.99%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -4,7 +4,7 @@ class Solution {
         long right=(long) Math.sqrt(c);
 
         while(left<=right){
-            long sum= left*left + right*right;
+            long sum= (left*left) + (right*right);
             if(sum==c){
                 return true;
             }else if(sum<c){
