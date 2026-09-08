abb523061f28c17b78189951d2a77e71da26aa12
Uday Katale <kataleuday@gmail.com>
2026-09-08 23:43:13 +0530

    Time: 2 ms (63.99%) | Memory: 43.7 MB (15.72%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!= goal.length()){
+            return false;
+        }
+        String newString= s+s;
+        if(newString.contains(goal)){
+            return true;
+        }
+        return false;
+    }
+}
