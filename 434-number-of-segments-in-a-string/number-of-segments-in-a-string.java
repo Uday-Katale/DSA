a766e277ec64fdc196e104560e324af8bf98dbf6
Uday Katale <kataleuday@gmail.com>
2026-09-14 22:55:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (14.71%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
@@ -1,12 +1,11 @@
 class Solution {
     public int countSegments(String s) {
-        int segmentsCount = 0;
+        int Count = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
-                segmentsCount++;
+                Count++;
             }
         }
-
-        return segmentsCount;
+        return Count;
     }
 }
