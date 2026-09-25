1318249ebe6f3d8c96906847a208745bffa4f609
Uday Katale <kataleuday@gmail.com>
2026-09-25 20:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (69.03%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean [] seen = new boolean[26];
-        for(char ch : sentence.toCharArray()){
-            seen[ch-'a']= true;
+        // boolean[] seen = new boolean[26];
+        // for (char ch : sentence.toCharArray()) {
+        //     seen[ch - 'a'] = true;
+        // }
+        // for (boolean b : seen) {
+        //     if (!b) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        if(sentence.length()<26){
+            return false;
         }
-        for(boolean b : seen){
-            if(!b){
+        for( char ch= 'a'; ch<='z'; ch++){
+            if(sentence.indexOf(ch)==-1){
                 return false;
             }
         }
